9f096f164f6651d1c8722c516035124061c96c5d
Anderson <acervante@ext.worten.pt>
2023-11-07 09:04:35 -0300

    fixed a code

--- utils/finder.go
+++ utils/finder.go
@@ -8,8 +8,6 @@ import (
 	"github.com/gabrielcervante/handler/types"
 )
 
-type withoutOutput[I any] func(ctx context.Context, i I, fn types.ErrorFunction[I]) error
-
 func FindFuncType[I, O any](ctx context.Context, input I, fnToFindType any) (O, error) {
 	if reflect.TypeOf(fnToFindType).NumIn() == 2 && reflect.TypeOf(fnToFindType).NumOut() == 2 {
 		return inputFunction[I, O](ctx, input, fnToFindType.(types.InputFunction[I, O]))
@@ -20,7 +18,7 @@ func FindFuncType[I, O any](ctx context.Context, input I, fnToFindType any) (O, 
 	}
 
 	if reflect.TypeOf(fnToFindType).NumIn() == 2 && reflect.TypeOf(fnToFindType).NumOut() == 1 {
-		return emptyOutput[I, O](ctx, input, fnToFindType.(types.ErrorFunction[I]), errorFunction[I])
+		return emptyOutput[I, O](ctx, input, fnToFindType.(types.ErrorFunction[I]))
 	}
 
 	panic("Sorry, you provided a wrong type " + fmt.Sprintf("%T", fnToFindType))
@@ -34,11 +32,7 @@ func outputFunction[O any](ctx context.Context, fn types.OutputFunction[O]) (O, 
 	return fn(ctx)
 }
 
-func emptyOutput[I, O any](ctx context.Context, i I, errFn types.ErrorFunction[I], fn withoutOutput[I]) (_ O, err error) {
-	err = fn(ctx, i, errFn)
+func emptyOutput[I, O any](ctx context.Context, i I, fn types.ErrorFunction[I]) (_ O, err error) {
+	err = fn(ctx, i)
 	return
-}
-
-func errorFunction[I any](ctx context.Context, i I, fn types.ErrorFunction[I]) error {
-	return fn(ctx, i)
 }
